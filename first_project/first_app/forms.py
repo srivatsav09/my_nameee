a7fcd31baaff80bc50a65400ee1939696879eff9
Srivatsav <srivatsav.vijay2003@gmail.com>
2023-07-13 21:30:27 +0530

    commit

--- first_project/first_app/forms.py
+++ first_project/first_app/forms.py
@@ -1,6 +1,6 @@
-from django import forms
+# from django import Forms
 
-class FormName(forms.form):
-    name = forms.CharField()
-    email = forms.EmailField()
-    text = forms.CharField(widget=forms.Textarea)
+# class FormName(forms.form):
+#     name = forms.CharField()
+#     email = forms.EmailField()
+#     text = forms.CharField(widget=forms.Textarea)
