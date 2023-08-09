161ce14c3acaa96a08179c04597bea128bf85352
Srivatsav <srivatsav.vijay2003@gmail.com>
2023-08-09 13:07:50 +0530

    commit

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,39 +1,77 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 #from first_app.models import Topic,Webpage,AccessRecord,User
+from first_app.forms import UserForm,UserProfileImfoForm
+
 from . import forms
 #from first_app.forms import NewUserForm
 # Create your views here.
 
 
 def index(request):
-    #webpages_list = AccessRecord.objects.order_by('date')
-    #date_dict = {'access_records':webpages_list}
-    #my_dict = {'insert_me' : "Hello i am from views.py!"}
-    #return render(request,'first_app/index.html',context=date_dict)
-    context_dict = {'text':'hello world','number':100}
-    return render(request,'first_app/index.html',context_dict)
-    # return render(request,'first_app/index.html')
+    return render(request,'first_app/index.html')
 
-def other(request):
-    return render(request,'first_app/other.html')
+def registration(request):
+    registered = False
+    if request.method =="POST":
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileImfoForm(data=request.POST)
 
-def relative(request):
-    return render(request,'first_app/rel_templates.html')
+        if user_form.is_valid() and profile_form.is_valid():
 
-def users(request):
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
 
-    form = NewUserForm()
+            profile = profile_form.save(commit=False)
+            profile.user = user
 
-    if request.method == "POST":
-        form = NewUserForm(request.POST)
+            if 'prof_pic' in request.FILES:
+                profile.prof_pic = request.FILES['prof_pic']
 
-        if form.is_valid():
-            form.save(commit=True)
-            return index(request)
+            profile.save()
+
+            registered = True
         else:
-            print("error form invalid")
-    return render(request,"first_app/users.html",{"form":form})
+            print(user_form.errors,profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileImfoForm()
+    
+    return render(request,'first_app/registration.html',
+                  {'user_form':user_form,
+                   'profile_form':profile_form,
+                   'registered':registered})
+
+
+# def index(request):
+#     #webpages_list = AccessRecord.objects.order_by('date')
+#     #date_dict = {'access_records':webpages_list}
+#     #my_dict = {'insert_me' : "Hello i am from views.py!"}
+#     #return render(request,'first_app/index.html',context=date_dict)
+#     context_dict = {'text':'hello world','number':100}
+#     return render(request,'first_app/index.html',context_dict)
+#     # return render(request,'first_app/index.html')
+
+# def other(request):
+#     return render(request,'first_app/other.html')
+
+# def relative(request):
+#     return render(request,'first_app/rel_templates.html')
+
+# def users(request):
+
+#     form = NewUserForm()
+
+#     if request.method == "POST":
+#         form = NewUserForm(request.POST)
+
+#         if form.is_valid():
+#             form.save(commit=True)
+#             return index(request)
+#         else:
+#             print("error form invalid")
+#     return render(request,"first_app/users.html",{"form":form})
 
 # def form_name_view(request):
 #     form = forms.FormName()
